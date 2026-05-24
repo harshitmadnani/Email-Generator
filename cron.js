a23e66d94df6cf709afc2f4971c87d39b793b163
Harshit Madnani <hmadnani4@gmail.com>
2026-05-24 16:51:08 +0530

    removed node-cron, using github actions

--- cron.js
+++ cron.js
@@ -1,19 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import cron from "node-cron";
 import { sendMail } from "./mailer.js";
 
-console.log("🚀 Cron service started...");
+console.log("💌 Running mail job...");
 
-// ✅ Runs daily at 6:30 AM IST
-cron.schedule(
-  "30 6 * * *",
-  async () => {
-    console.log("⏰ Sending Good Morning Mail...");
-    await sendMail();
-  },
-  {
-    timezone: "Asia/Kolkata",
-  }
-);
+sendMail();
